3ff7c3a6e08e0cae7adbd2b7e602fd9470763774
Walt Boxwell <waltboxwell@gmail.com>
2022-03-30 18:54:57 -0500

    Added basic html using node.js

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template')
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+   if (err) throw err;
+   console.log('Portfolio complete! Checkout index.html to see the output!')
+});   
+
